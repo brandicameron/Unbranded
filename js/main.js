c75d20d3a63d02b7f2c4aa12640f989040c0f6ff
Brandi Cameron <hello@brandicameron.com>
2021-01-04 10:29:00 -0600

    Changed large view to click instead of hover

--- js/main.js
+++ js/main.js
@@ -29,8 +29,10 @@ $(document).ready(function () {
 const body = document.querySelector('body');
 
 function viewLarge(e) {
+	
+	let lgImage;
+	
 	if (e.target.classList.contains('cards')) {
-		
 		const imgClicked = e.target.src;
 		const frontImage = imgClicked.replace('-3','-2');
 		const backImage = imgClicked.replace('-2','-3');
@@ -51,12 +53,62 @@ function viewLarge(e) {
 		
 		const lgImage = document.createElement('img');
 		lgImage.src = frontImage;
-		lgImage.onmouseover = function() {lgImage.src = backImage;};
-		lgImage.onmouseout = function() {lgImage.src = frontImage;};
 		lgImage.setAttribute('alt', imageTitle);
 		imgContainer.appendChild(lgImage);
+		
+		const thumbnailContainer = document.createElement('div');
+		thumbnailContainer.classList.add('thumbnails');
+		lgViewContainer.appendChild(thumbnailContainer);
+		
+		const thumbFront = document.createElement('img');
+		thumbFront.classList.add('thumbnail');
+		thumbFront.setAttribute('alt', imageTitle);
+		thumbFront.src = frontImage;
+		thumbnailContainer.appendChild(thumbFront);
+		
+		const thumbBack = document.createElement('img');
+		thumbBack.classList.add('thumbnail');
+		thumbBack.setAttribute('alt', imageTitle);
+		thumbBack.src = backImage;
+		thumbnailContainer.appendChild(thumbBack);
+		
+		thumbnailContainer.addEventListener('click', (e) => {
+			if (e.target.classList.contains('thumbnail')) {
+				lgImage.src = e.target.src;
+			}
+		})
 	}
 }
+
+//function viewLarge(e) {
+//	if (e.target.classList.contains('cards')) {
+//		
+//		const imgClicked = e.target.src;
+//		const frontImage = imgClicked.replace('-3','-2');
+//		const backImage = imgClicked.replace('-2','-3');
+//		const imageTitle = e.target.nextElementSibling.textContent;
+//		
+//		const lgViewContainer = document.createElement('div');
+//		lgViewContainer.classList.add('large-view');
+//		body.appendChild(lgViewContainer);
+//
+//		const closeBtn = document.createElement('div');
+//		closeBtn.classList.add('close');
+//		closeBtn.textContent = "X";
+//		lgViewContainer.appendChild(closeBtn);
+//
+//		const imgContainer = document.createElement('div');
+//		imgContainer.classList.add('large-image');
+//		lgViewContainer.appendChild(imgContainer);
+//		
+//		const lgImage = document.createElement('img');
+//		lgImage.src = frontImage;
+//		lgImage.onmouseover = function() {lgImage.src = backImage;};
+//		lgImage.onmouseout = function() {lgImage.src = frontImage;};
+//		lgImage.setAttribute('alt', imageTitle);
+//		imgContainer.appendChild(lgImage);
+//	}
+//}
 
 
 
